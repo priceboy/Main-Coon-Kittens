7889c0093e67007bca1931bbdb8fe077bdcf3839
priceboy <psheutang@gmail.com>
2026-09-10 22:11:46 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -35,8 +35,10 @@ LANGUAGE_CODE = "en-us"
 TIME_ZONE = "Africa/Douala"
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
