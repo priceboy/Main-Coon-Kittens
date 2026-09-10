721a8481ae6269f0406314db6696de0d7f580926
priceboy <psheutang@gmail.com>
2026-09-10 22:05:02 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "presentation-only-change-before-production"
 DEBUG = True
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "testserver"]
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'testserver',
+    '.vercel.app',
+]
 INSTALLED_APPS = [
     "django.contrib.admin", "django.contrib.auth", "django.contrib.contenttypes",
     "django.contrib.sessions", "django.contrib.messages", "django.contrib.staticfiles",
